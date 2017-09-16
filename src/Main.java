838baac0442a34c294aba3dc0b49aa908abdecd7
StephenJak <32010405+StephenJak@users.noreply.github.com>
2017-09-16 14:43:45 +0530

    third submission

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
-		
+		int n=10;
+		StudentGroup sg=new StudentGroup(n);
 	}
 
 }
